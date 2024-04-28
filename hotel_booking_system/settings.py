21e4713533df76928ffdc986d03bfdeb384945e9
SyedaShafi <syedashafi4@gmail.com>
2024-04-29 02:56:31 +0600

    csrf added

--- hotel_booking_system/settings.py
+++ hotel_booking_system/settings.py
@@ -18,7 +18,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+CSRF_TRUSTED_ORIGINS = ['https://hotel-api-99rb.onrender.com',  'https://*.127.0.0.1' ]
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
